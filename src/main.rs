150a2c401ab135f67431acd74674ffcbe911f7aa
mades eladrian <mades.eladrian@gmail.com>
2024-01-22 22:24:05 -0300

    add function that convert celsius in fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     let fahrenheit_to_celsius: f64 = fahrenheit_to_celsius(32.0);
-
     println!("The temperature in Celsius is: {fahrenheit_to_celsius}°C");
+    let celsius_to_fahrenheit: f64 = celsius_to_fahrenheit(0.0);
+    println!("The temperature in Fahrenheit is: {celsius_to_fahrenheit}°F");
+}
+
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    celsius * 9.0 / 5.0 + 32.0
 }
 
 fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
